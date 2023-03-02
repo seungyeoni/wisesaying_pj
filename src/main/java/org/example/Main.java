119be9f103fd97cf63e706c7af385ce3fd7036f5
seungyeoni <dnri33@naver.com>
2023-03-02 10:07:29 +0900

    5단계 완료

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,19 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int count = 0;
+        List<Wise> write = new ArrayList<>();
         System.out.println("== 명언 앱 ==");
         while(true){
             System.out.printf("명령) ");
-            String i = sc.nextLine();
+            String i = sc.nextLine().trim();
             if(i.equals("종료")){
                 break;
             }else if(i.equals("등록")){
@@ -18,9 +22,33 @@ public class Main {
                 System.out.printf("작가 : ");
                 String i3 = sc.nextLine();
                 count++;
+                write.add(new Wise(count,i2,i3));
                 System.out.println(count+"번 명언이 등록되었습니다.");
+            }else if(i.equals("목록")){
+                System.out.println("번호 / 작가 / 명언");
+                System.out.println("--------------------------");
+                Collections.reverse(write);
+                for (Wise w :write){
+                    System.out.println(w.toString());
+                }
             }
         }
         sc.close();
+    }
+}
+class Wise{
+    private int id;
+    private String wise;
+    private String writer;
+
+    public Wise(int id, String wise, String writer){
+        this.id = id;
+        this.wise = wise;
+        this.writer = writer;
+    }
+
+    @Override
+    public String toString() {
+        return this.id+ " / "+this.writer+" / "+this.wise;
     }
 }
